11d45a53f45679af515a329e8e1488143487b011
Andrey Levchenko <andreiilevchenko@gmail.com>
2023-06-21 18:04:39 +0300

    init nodemailer

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -1,4 +1,6 @@
 import nodemailer from "nodemailer";
+import hbs from "nodemailer-express-handlebars";
+import * as path from "path";
 
 class EmailService {
   private transporter;
@@ -8,22 +10,44 @@ class EmailService {
       service: "gmail",
       auth: {
         user: "andreiilevchenko@gmail.com",
-        pass: "dnsfddbiclfinoov",
+        pass: "duixtjkfcwkxuzez",
       },
     });
     const hbsOptions = {
-      viewEngine:{
-        extname:'.hbs',
-        defaultLayout:'main',
-      }
-    }
+      viewEngine: {
+        extname: ".hbs",
+        defaultLayout: "main",
+        layoutsDir: path.join(
+          process.cwd(),
+          "src",
+          "email-templates",
+          "layouts"
+        ),
+        partialsDir: path.join(
+          process.cwd(),
+          "src",
+          "email-templates",
+          "partials"
+        ),
+      },
+      viewPath: path.join(process.cwd(), "src", "email-templates", "views"),
+      extName: ".hbs",
+    };
+    this.transporter.use("compile", hbs(hbsOptions));
   }
   public async sendMail(email: string) {
-    return this.transporter.sendMail({
-      to: email,
+    const mailOptions = {
       subject: "HELLO!!!!!!",
-      html: "<div>Вітаю. Якщо ви отримали це повідомлення, невідкладно сповістіть відправника про це. Дякую. Гарного дня!!!</div>",
-    });
+      template: "register",
+    };
+    return this.transporter.sendMail(
+      mailOptions
+      //     {
+      //   to: email,
+      //   subject: "HELLO!!!!!!",
+      //   html: "<div>Вітаю. Якщо ви отримали це повідомлення, невідкладно сповістіть відправника про це. Дякую. Гарного дня!!!</div>",
+      // }
+    );
   }
 }
 
